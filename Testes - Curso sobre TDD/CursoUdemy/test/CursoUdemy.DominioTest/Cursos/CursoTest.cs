17cb68a600542be29bc2f4b105b88a82469165a1
Joan Vitor <j.v.m.j@hotmail.com>
2021-05-14 16:45:32 -0300

    Adicionando validações na classe Curso + Criando Enum para Publico Alvo + Criando metodo de extensao para classe ArgumentException com a classe AssertExtension + Validando através do Asssert.Throws se o curso tem os campos obrigatorios preenchidos.

--- Testes - Curso sobre TDD/CursoUdemy/test/CursoUdemy.DominioTest/Cursos/CursoTest.cs
+++ Testes - Curso sobre TDD/CursoUdemy/test/CursoUdemy.DominioTest/Cursos/CursoTest.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using CursoUdemy.DominioTest._Util;
+using ExpectedObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,6 +7,13 @@ using Xunit;
 
 namespace CursoUdemy.DominioTest.Cursos
 {
+    // "Eu, enquanto adm, quero criar e editar cursos para que sejam abertas matriculas para o mesmo.
+
+    // Criterio de aceite
+
+    // 1 - Criar um curso com nome, carga horaria, publico alvo e valor do curso
+    // 2 - As opções para publico alvo são: estudante, universitario, empregado e empreendedor
+    // 3 - Todos os campos do curso são obrigatorios
     public class CursoTest
     {
         [Fact]
@@ -31,7 +39,7 @@ namespace CursoUdemy.DominioTest.Cursos
             {
                 Nome = "Informática Básica",
                 CargaHoraria = (double) 80,
-                PublicoAlvo = "Estudante",
+                PublicoAlvo = PublicoAlvo.Estudante,
                 ValorCurso = (double) 950
             };
 
@@ -42,15 +50,112 @@ namespace CursoUdemy.DominioTest.Cursos
             CursoEsperado.ToExpectedObject().ShouldMatch(curso);
         }
 
+        // Aula sobre - Tratando parametros inválidos
+        // Usa Theory para criar testes parametrizados com inlinedata, pq se fosse o FACT iria precisar ter 2 metodos identicos (1 para tratar null e outro vazio "")
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void CursoNaoPodeTerNomeInvalido(string nomeInvalido)
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                ValorCurso = (double)950
+            };
+
+            // Valida se houve uma exceção do tipo ArgumentException
+            // Se não tiver exceção ou o exception for diferente do ArgumentException, o teste irá falhar, na criação do objeto
+            //var messagem = Assert.Throws<ArgumentException>(() 
+            //    => new Curso(
+            //        nomeInvalido, 
+            //        CursoEsperado.CargaHoraria, 
+            //        CursoEsperado.PublicoAlvo, 
+            //        CursoEsperado.ValorCurso)
+            //    ).Message;
+            //Assert.Equal("Nome inválido", messagem);
+
+            // Utilizando a Extensão criada na classe AssertExtension
+            Assert.Throws<ArgumentException>(()
+                => new Curso(
+                    nomeInvalido,
+                    CursoEsperado.CargaHoraria,
+                    CursoEsperado.PublicoAlvo,
+                    CursoEsperado.ValorCurso)
+                ).ComMensagem("Nome inválido");
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void CursoNaoDeveTerCargaHorariaMenorQue1(double CargaHorariaInvalida)
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                ValorCurso = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(()
+                => new Curso(
+                    CursoEsperado.Nome, 
+                    CargaHorariaInvalida, 
+                    CursoEsperado.PublicoAlvo, 
+                    CursoEsperado.ValorCurso)
+                ).ComMensagem("Carga horária inválida");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-2)]
+        [InlineData(-100)]
+        public void CursoNaoDeveTerValorMenorQue1(double ValorInvalido)
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double)80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                ValorCurso = (double)950
+            };
+
+            Assert.Throws<ArgumentException>(()
+                => new Curso(
+                    CursoEsperado.Nome, 
+                    CursoEsperado.CargaHoraria,
+                    CursoEsperado.PublicoAlvo, 
+                    ValorInvalido)
+                ).ComMensagem("Valor inválido");
+        }
+
+        public enum PublicoAlvo
+        {
+            Estudante,
+            Universitario,
+            Empregado,
+            Empreendedor
+        }
+
         public class Curso
         {
             public string Nome { get; private set; }
             public double CargaHoraria { get; private set; }
-            public string PublicoAlvo { get; private set; }
+            public PublicoAlvo PublicoAlvo { get; private set; }
             public double ValorCurso { get; private set; }
 
-            public Curso(string nome, double CargaHoraria, string publicoAlvo, double valorCurso)
+            public Curso(string nome, double CargaHoraria, PublicoAlvo publicoAlvo, double valorCurso)
             {
+                if (string.IsNullOrEmpty(nome)) throw new ArgumentException("Nome inválido");
+
+                if (CargaHoraria < 1) throw new ArgumentException("Carga horária inválida");
+
+                if (valorCurso < 1) throw new ArgumentException("Valor inválido");
+
                 this.Nome = nome;
                 this.CargaHoraria = CargaHoraria;
                 this.PublicoAlvo = publicoAlvo;
---
+++ Testes - Curso sobre TDD/CursoUdemy/test/CursoUdemy.DominioTest/_Util/AssertExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoUdemy.DominioTest._Util
+{
+    // Classe de extensão tem que ser estatica
+    public static class AssertExtension
+    {
+        // Criando metodo de extensão para a classe ArgumentException
+        // Como eu sei disso ? Porque o parametro ArgumentException, antes dele tem o this
+        public static void ComMensagem(this ArgumentException exception, string mensagem)
+        {
+            if (exception.Message == mensagem)
+                Assert.True(true);
+            else
+                Assert.False(true, $"Esperava a mensagem {mensagem}");
+        }
+    }
+}
