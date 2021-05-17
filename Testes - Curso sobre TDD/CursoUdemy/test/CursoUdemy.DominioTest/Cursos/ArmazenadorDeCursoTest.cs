7dd63e10b8686b9339dcb88ddb0b22ef3837a0b8
Joan Vitor <j.v.m.j@hotmail.com>
2021-05-17 10:26:04 -0300

    Melhorando verificaçao dos mocks

--- Testes - Curso sobre TDD/CursoUdemy/test/CursoUdemy.DominioTest/Cursos/ArmazenadorDeCursoTest.cs
+++ Testes - Curso sobre TDD/CursoUdemy/test/CursoUdemy.DominioTest/Cursos/ArmazenadorDeCursoTest.cs
@@ -1,4 +1,5 @@
-﻿using CursoUdemy.Dominio.Cursos;
+﻿using Bogus;
+using CursoUdemy.Dominio.Cursos;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -9,31 +10,49 @@ namespace CursoUdemy.DominioTest.Cursos
 {
     public class ArmazenadorDeCursoTest
     {
+        private readonly ArmazenadorDeCurso _armazenadorDeCurso;
+        private readonly CursoDto _cursoDTO;
+        private readonly Mock<ICursoRepositorio> _cursoRepositorioMock;
+
+        public ArmazenadorDeCursoTest()
+        {
+            var fake = new Faker();
+
+            _cursoDTO = new CursoDto
+            {
+                Nome = fake.Random.Words(),
+                Descricao = fake.Lorem.Paragraph(),
+                CargaHoraria = fake.Random.Double(50, 1000),
+                PublicoAlvoId = 1,
+                Valor = fake.Random.Double(1000, 2000)
+            };
+
+            _cursoRepositorioMock = new Mock<ICursoRepositorio>();
+
+            _armazenadorDeCurso = new ArmazenadorDeCurso(_cursoRepositorioMock.Object);
+        }
+
         [Fact]
         public void DeveAdicionar()
         {
-            var cursoDTO = new CursoDto
-            {
-                Nome = "curso a",
-                Descricao = "Descricao",
-                CargaHoraria = 80,
-                PublicoAlvoId = 1,
-                Valor = 850.00
-            };
+            _armazenadorDeCurso.Armazenar(_cursoDTO);
 
-            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
-
-            var armazenadorDeCurso = new ArmazenadorDeCurso(cursoRepositorioMock.Object);
-
-            armazenadorDeCurso.Armazenar(cursoDTO);
-
-            cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+            //Pode ser que esteja passando um Curso X e na hora de instanciar tenha o Curso Y, logo está errado
+            // Então utiliza o Is
+            _cursoRepositorioMock.Verify(r => r.Adicionar(
+                It.Is<Curso>(
+                    c => c.Nome == _cursoDTO.Nome
+                    && c.Descricao == _cursoDTO.Descricao
+                )
+            ), Times.AtLeast(2));
+            // Times.AtLeast(2) - espera que o metodo seja chamado pelo menos duas vezes
         }
     }
 
     public interface ICursoRepositorio
     {
         void Adicionar(Curso curso);
+        void Atualizar(Curso curso);
     }
 
     public class ArmazenadorDeCurso
@@ -54,6 +73,7 @@ namespace CursoUdemy.DominioTest.Cursos
                 PublicoAlvo.Estudante,
                 cursoDto.Valor);
 
+            _cursoRepositorio.Adicionar(curso);
             _cursoRepositorio.Adicionar(curso);
         }
     }
