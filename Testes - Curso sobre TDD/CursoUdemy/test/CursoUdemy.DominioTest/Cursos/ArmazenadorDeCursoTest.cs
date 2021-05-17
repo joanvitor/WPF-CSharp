8bbf95c741fa54a1eaae7254e33fad3aa34202cc
Joan Vitor <j.v.m.j@hotmail.com>
2021-05-17 09:14:26 -0300

    Iniciando com mocks

---
+++ Testes - Curso sobre TDD/CursoUdemy/test/CursoUdemy.DominioTest/Cursos/ArmazenadorDeCursoTest.cs
@@ -0,0 +1,69 @@
+﻿using CursoUdemy.Dominio.Cursos;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoUdemy.DominioTest.Cursos
+{
+    public class ArmazenadorDeCursoTest
+    {
+        [Fact]
+        public void DeveAdicionar()
+        {
+            var cursoDTO = new CursoDto
+            {
+                Nome = "curso a",
+                Descricao = "Descricao",
+                CargaHoraria = 80,
+                PublicoAlvoId = 1,
+                Valor = 850.00
+            };
+
+            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
+
+            var armazenadorDeCurso = new ArmazenadorDeCurso(cursoRepositorioMock.Object);
+
+            armazenadorDeCurso.Armazenar(cursoDTO);
+
+            cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+        }
+    }
+
+    public interface ICursoRepositorio
+    {
+        void Adicionar(Curso curso);
+    }
+
+    public class ArmazenadorDeCurso
+    {
+        private readonly ICursoRepositorio _cursoRepositorio;
+
+        public ArmazenadorDeCurso(ICursoRepositorio cursoRepositorio)
+        {
+            _cursoRepositorio = cursoRepositorio;
+        }
+
+        public void Armazenar(CursoDto cursoDto)
+        {
+            var curso = new Curso(
+                cursoDto.Nome,
+                cursoDto.Descricao,
+                cursoDto.CargaHoraria,
+                PublicoAlvo.Estudante,
+                cursoDto.Valor);
+
+            _cursoRepositorio.Adicionar(curso);
+        }
+    }
+
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double CargaHoraria { get; set; }
+        public int PublicoAlvoId { get; set; }
+        public double Valor { get; set; }
+    }
+}
